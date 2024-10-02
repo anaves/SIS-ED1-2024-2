a1de06e8b8baa96d420aa95c9cc942a3adabaad2
Alysson Naves <alysson.naves@ectare.com.br>
2024-10-02 20:22:11 -0300

    merge sort incompleto

--- ordenacao/merge_sort.py
+++ ordenacao/merge_sort.py
@@ -21,11 +21,26 @@ def intercala(inicio, meio, fim, lista):
         j = j+1
     return lista_ordenada
 
+
+def mergeSort(inicio, fim, lista):
+    if inicio < fim - 1:
+        meio = (inicio+fim)//2
+        print(f"ini={inicio} meio={meio} fim={fim}")
+        print(lista[inicio:fim])
+        input("pause 1: ")
+        mergeSort(inicio, meio, lista)
+        input("pause 2: ")
+        mergeSort(meio, fim, lista)
+        
+
 # main: principal
 if __name__ == '__main__':
-    valores = [1,3,6,7,0,8,11,12]
-    inicio = 0
-    fim = len(valores)
-    meio = (inicio+fim)//2
-    ordenada = intercala(inicio, meio, fim, valores)
-    print(ordenada)
+    valores = [29, 10, 14, 37, 13]
+    mergeSort(0, 5, valores)
+    print(valores)
+    # valores = [1,3,6,7,0,8,11,12]
+    # inicio = 0
+    # fim = len(valores)
+    # meio = (inicio+fim)//2
+    # ordenada = intercala(inicio, meio, fim, valores)
+    # print(ordenada)
